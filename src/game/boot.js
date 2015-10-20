0a18d55cb46cce329178f60332bc24a248e2e8f3
Ivan Montiel <idmontie@asu.edu>
2015-10-19 22:23:02 -0700

    Even better resize

--- src/game/boot.js
+++ src/game/boot.js
@@ -1,3 +1,5 @@
+var Settings = require('core/settings');
+
 function Boot( game ) {};
 
 Boot.prototype.preload = function () {
@@ -6,14 +8,15 @@ Boot.prototype.preload = function () {
 
 Boot.prototype.create = function () {
   var game = this;
+  var ratio = Settings.size.width / Settings.size.height;
   var resize = function () {
     var height = $(window).height() - 2 * $('body').css("margin-top").replace("px", "");
     var width = $(window).width() - 2 * $('body').css("margin-left").replace("px", "");
-      
+
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    game.scale.minWidth = 320;
-    game.scale.minHeight = 480;
-    game.scale.maxWidth = width;
+    game.scale.minWidth = height * ratio;
+    game.scale.minHeight = height;
+    game.scale.maxWidth = height * ratio;
     game.scale.maxHeight = height;
     game.scale.refresh();
   }
@@ -21,7 +24,6 @@ Boot.prototype.create = function () {
   $(window).resize(resize);
 
   resize();
-  
     
   if (this.renderType === Phaser.WEBGL) {
     this.renderer.resize(width, height);
