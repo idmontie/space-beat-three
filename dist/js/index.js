44b20f4955a2c76c623d41c420625eb55838972b
Ivan Montiel <idmontie@asu.edu>
2015-10-19 22:04:46 -0700

    Better resize

--- dist/js/index.js
+++ dist/js/index.js
@@ -148,12 +148,27 @@
 	}
 
 	Boot.prototype.create = function () {
-	  this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-	  this.scale.minWidth = 320;
-	  this.scale.minHeight = 480;
-	  this.scale.maxWidth = 768;
-	  this.scale.maxHeight = 1152;
-	  this.scale.refresh();
+	  var game = this;
+	  var resize = function () {
+	    var height = $(window).height() - 2 * $('body').css("margin-top").replace("px", "");
+	    var width = $(window).width() - 2 * $('body').css("margin-left").replace("px", "");
+	      
+	    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+	    game.scale.minWidth = 320;
+	    game.scale.minHeight = 480;
+	    game.scale.maxWidth = width;
+	    game.scale.maxHeight = height;
+	    game.scale.refresh();
+	  }
+
+	  $(window).resize(resize);
+
+	  resize();
+	  
+	    
+	  if (this.renderType === Phaser.WEBGL) {
+	    this.renderer.resize(width, height);
+	  }
 
 	  this.state.start( 'Preloader' );
 	}
