4bdc10faaf8d2cfbecc24cc212e49ff10b478ae6
Ivan Montiel <idmontie@asu.edu>
2015-10-19 21:20:06 -0700

    Initial version

--- dist/js/index.js
+++ dist/js/index.js
@@ -239,10 +239,20 @@
 	function StartMenu( game ) {}
 
 	StartMenu.prototype.create = function () {
-	  // TODO set up start menu
+	  var style = { font: '65px Arial', fill: '#ffffff', align: 'center'};
 
-	  // TODO delete this line once the start menu is set up
-	  this.state.start( 'Game' );
+	  this.titleText = this.game.add.text(this.game.world.centerX, 300, 'Space Beat Three', style);
+	  this.titleText.anchor.setTo(0.5, 0.5);
+
+	  this.instructionsText = this.game.add.text(this.game.world.centerX, 400, 'Click anywhere to play', { font: '16px Arial', fill: '#ffffff', align: 'center'});
+	  this.instructionsText.anchor.setTo(0.5, 0.5);
+
+	}
+
+	StartMenu.prototype.update = function () {
+	  if(this.game.input.activePointer.justPressed()) {
+	    this.startGame();
+	  }
 	}
 
 	StartMenu.prototype.startGame = function () {
